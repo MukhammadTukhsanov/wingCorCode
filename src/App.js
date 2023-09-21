2662aff44f3a0b78ac296b5ef442608caa541fe3
Mukhammad <muxammadhotamo@gmail.com>
2023-09-21 12:59:37 +0500

    new changes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import About from './components/About';
-import Features from './components/Features';
-import Footer from './components/Footer';
-import Header from './components/Header';
-import Navbar from './components/Navbar';
-import Solution from './components/Solution';
 
 function App() {
   return (
     <>
-    <About />
+    <Routes>
+      <Route path='<About />' />
+    </Routes>
     </>
   );
 }
